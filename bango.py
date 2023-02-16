ccffb5d89e0ba9d5ade8a6daf6cf95c0ddfd50aa
FlatBananoTheory <62030727+FlatBananoTheory@users.noreply.github.com>
2023-02-16 05:14:59 -0600

    Update bango.py
    * Added support for emojis with transparent backgrounds
    * Added unique CardID text to each card

--- bango.py
+++ bango.py
@@ -2,8 +2,7 @@ import glob
 import os
 import time
 from random import randint
-from PIL import Image
-import image
+from PIL import Image, ImageDraw, ImageFont
 
 
 def printAll(strings):
@@ -26,7 +25,6 @@ def getNewRandomIndex(max, currentIndices):
 
 
 def generateCard(nbSlots, maxIndex):
-
     currentCard = []
 
     for i in range(0, nbSlots):
@@ -36,7 +34,6 @@ def generateCard(nbSlots, maxIndex):
 
 
 def generateAllCardIndices(nbSlots, nbImages, nbCards):
-
     allCards = []
 
     for i in range(0, nbCards):
@@ -50,13 +47,12 @@ def generateAllCardIndices(nbSlots, nbImages, nbCards):
     return allCards
 
 
-def generateCardImage(indices, imagePaths, nbRows, nbCols, skipMiddle, bgPaths, positions):
-
+def generateCardImage(indices, imagePaths, nbRows, nbCols, skipMiddle, bgPaths, positions, prefix, cardNum):
     sources = []
     maxSize = [0, 0]
 
     for index in indices:
-        src = Image.open(imagePaths[index])
+        src = Image.open(imagePaths[index]).convert('RGBA')
         sources.append(src)
 
         if (src.size[0] > maxSize[0]):
@@ -66,9 +62,9 @@ def generateCardImage(indices, imagePaths, nbRows, nbCols, skipMiddle, bgPaths, 
             maxSize[1] = src.size[1]
 
     if len(bgPaths) > 0:
-        image = Image.open(bgPaths[0])
+        image = Image.open(bgPaths[0]).convert('RGBA')
     else:
-        image = Image.new("RGBA", (maxSize[0] * nbCols, maxSize[1] * nbRows))
+        image = Image.new("RGBA", (maxSize[0] * nbCols, maxSize[1] * nbRows), (0, 0, 0, 0))
 
     currentIndex = 0
     try:
@@ -81,74 +77,84 @@ def generateCardImage(indices, imagePaths, nbRows, nbCols, skipMiddle, bgPaths, 
                     else:
                         position = positions[currentIndex]
 
-                    image.paste(sources[currentIndex], position)
+                    image.alpha_composite(sources[currentIndex], dest=position)
                     currentIndex = currentIndex + 1
+
+        # Add text overlay to uniquely identify the card
+        draw = ImageDraw.Draw(image)
+        font = ImageFont.truetype('FilsonProRegular.otf', 24)
+        draw.text((25, 500), '{} {}'.format(prefix, cardNum), (255, 221, 17), font=font)
+
     except:
         print(currentIndex)
+
     return image
 
 
-################################
-# script folder
+# Script directory
 pwd = os.path.dirname(__file__)
 
-# list of image paths
-srcBackgrounds = pwd + '/images/background'
-srcImages = pwd + '/images/squares'
-outImages = pwd + '/result/' + time.strftime("%Y%m%d-%H%M%S") + '/'
-bgPaths = glob.glob(srcBackgrounds + '/*')
-imagePaths = glob.glob(srcImages + '/*')
+# List of image paths
+srcBackgrounds = os.path.join(pwd, 'images', 'background')
+srcImages = os.path.join(pwd, 'images', 'squares')
+outImages = os.path.join(pwd, 'result', time.strftime("%Y%m%d-%H%M%S"))
+bgPaths = glob.glob(os.path.join(srcBackgrounds, '*'))
+imagePaths = glob.glob(os.path.join(srcImages, '*'))
 nbImages = len(imagePaths)
 
 print(nbImages, "images found")
 
-# number of images in each card
+# Number of images in each card
 nbSlots = 25
 
-# number of cards
-nbCards = 1
+# Number of cards
+nbCards = 1000
 
-# number of rows
+# Number of rows
 nbRows = 5
 
-# number of columns
+# Number of columns
 nbCols = 5
 
-# skip middle
+# Skip middle
 skipMiddle = False
 
-# pixel offset 
-y_offset = -145
+# Y Pixel y_offset
+y_offset = -90
 x_offset = 10
 
 # Square positions in background
 squarePositions = [
-    (81 + x_offset, 892 + y_offset), (273 + x_offset, 892 + y_offset), (465  + x_offset, 892 +
-                                              y_offset), (657 + x_offset, 892 + y_offset), (849 + x_offset, 892 + y_offset),
+    (81 + x_offset, 892 + y_offset), (273 + x_offset, 892 + y_offset), (465 + x_offset, 892 +
+                                                                         y_offset), (657 + x_offset, 892 + y_offset), (849 + x_offset, 892 + y_offset),
     (81 + x_offset, 1084 + y_offset), (273 + x_offset, 1084 + y_offset), (465 + x_offset, 1084 +
-                                                y_offset), (657 + x_offset, 1084 + y_offset), (849 + x_offset, 1084 + y_offset),
+                                                                         y_offset), (657 + x_offset, 1084 + y_offset), (849 + x_offset, 1084 + y_offset),
     (81 + x_offset, 1276 + y_offset), (273 + x_offset, 1276 + y_offset),  (465 + x_offset, 1276 +
-                                                 y_offset), (657 + x_offset, 1276 + y_offset), (849 + x_offset, 1276 + y_offset),
+                                                                         y_offset), (657 + x_offset, 1276 + y_offset), (849 + x_offset, 1276 + y_offset),
     (81 + x_offset, 1468 + y_offset), (273 + x_offset, 1468 + y_offset), (465 + x_offset, 1468 +
-                                                y_offset), (657 + x_offset, 1468 + y_offset), (849 + x_offset, 1468 + y_offset),
+                                                                         y_offset), (657 + x_offset, 1468 + y_offset), (849 + x_offset, 1468 + y_offset),
     (81 + x_offset, 1660 + y_offset), (273 + x_offset, 1660 + y_offset), (465 + x_offset, 1660 +
-                                                y_offset), (657 + x_offset, 1660 + y_offset), (849 + x_offset, 1660 + y_offset)
+                                                                         y_offset), (657 + x_offset, 1660 + y_offset), (849 + x_offset, 1660 + y_offset)
 ]
+
+# Text overlay prefix
+prefix = "CardID:"
 
 print("Generating {} cards with {} indices each".format(nbCards, nbSlots))
 allCards = generateAllCardIndices(nbSlots, nbImages, nbCards)
 
-
-if (os.path.exists(outImages) == False):
+if not os.path.exists(outImages):
     os.makedirs(outImages)
 
 cardNum = 0
 
-
 for indices in allCards:
     result = generateCardImage(
-        indices, imagePaths, nbRows, nbCols, skipMiddle, bgPaths, squarePositions)
-    result.save(
-        outImages + 'FuckoBango69-R1C{}.png'.format(cardNum), "PNG")
-    print("indices: " + str(indices))
-    cardNum = cardNum + 1
+        indices, imagePaths, nbRows, nbCols, skipMiddle, bgPaths, squarePositions, prefix, cardNum)
+
+    if result:
+        result.save(os.path.join(outImages, 'Carnivalentines_Bango_R4C{}.png'.format(cardNum)), "PNG")
+        print("creating card #" + str(cardNum) + ": " + str(indices))
+        cardNum += 1
+    else:
+        print("Error generating card #" + str(cardNum))
